accebb8aac506e4a3f8b62551ecc01814a389941
Baur Kereibayev <kbaur775@gmail.com>
2020-12-13 17:55:37 -0600

    Adding environmennt var for db

--- server.js
+++ server.js
@@ -7,7 +7,11 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/workout", { useNewUrlParser: true });
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+  useNewUrlParser: true,
+  useFindAndModify: false,
+  useUnifiedTopology: true
+});
 
 app.use(require("./routes/html-routes.js"))
 app.use(require("./routes/api-routes.js"))
